d15a555372e12e290a533670c6803350e6841236
André Schreck <andre.schreck@gmail.com>
2018-03-04 22:12:43 +0100

    removed unused code

--- app/src/main/java/team/lunch/planner/IndexController.java
+++ app/src/main/java/team/lunch/planner/IndexController.java
@@ -1,29 +1,17 @@
 package team.lunch.planner;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import team.lunch.planner.team.domain.Team;
-import team.lunch.planner.team.domain.TeamService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
-    private final TeamService teamService;
-
     @GetMapping("/")
-    public String index(Model model) {
-        List<Team> teams = teamService.determineAllTeams();
-
-        model.addAttribute("teams", teams);
-
+    public String index() {
         return "index";
     }
-
 
 }
