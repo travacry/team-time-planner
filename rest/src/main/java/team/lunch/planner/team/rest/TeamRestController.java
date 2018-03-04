d3addc8496c1f2adb2e80d87236a5d1560d7d0ee
André Schreck <andre.schreck@gmail.com>
2018-03-04 21:54:52 +0100

    trigger deployment

--- rest/src/main/java/team/lunch/planner/team/rest/TeamRestController.java
+++ rest/src/main/java/team/lunch/planner/team/rest/TeamRestController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import team.lunch.planner.AbstractController;
 
 @RestController
 @ExposesResourceFor(TeamDTO.class)
 @RequestMapping("/api/teams")
 @RequiredArgsConstructor
-public class TeamRestController {
+public class TeamRestController extends AbstractController {
 
 }
