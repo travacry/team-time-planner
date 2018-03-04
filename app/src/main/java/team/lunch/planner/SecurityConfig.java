758c2921af761d0ed4805edf5b9d30317f7a69d6
André Schreck <andre.schreck@gmail.com>
2018-03-04 21:20:30 +0100

    angular

--- app/src/main/java/team/lunch/planner/SecurityConfig.java
+++ app/src/main/java/team/lunch/planner/SecurityConfig.java
@@ -45,6 +45,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .mvcMatchers(HttpMethod.OPTIONS).permitAll()
+            .mvcMatchers(HttpMethod.GET, "/index").permitAll()
+            .mvcMatchers(HttpMethod.GET, "/").permitAll()
+            .mvcMatchers(HttpMethod.GET, "/js/**").permitAll()
+            .mvcMatchers(HttpMethod.GET, "/css/**").permitAll()
+            .mvcMatchers(HttpMethod.GET, "/favicon.ico").permitAll()
             .mvcMatchers(HttpMethod.POST, "/api/users").permitAll()
                 .anyRequest().fullyAuthenticated();
 //                .and()
