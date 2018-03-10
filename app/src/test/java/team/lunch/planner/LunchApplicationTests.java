ef813cff2ece521049a940472b560c62dcd91243
André Schreck <andre.schreck@gmail.com>
2018-03-10 15:56:49 +0100

    merge to maven

--- app/src/test/java/team/lunch/planner/LunchApplicationTests.java
+++ app/src/test/java/team/lunch/planner/LunchApplicationTests.java
@@ -1,11 +1,5 @@
 package team.lunch.planner;
 
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.Arrays;
-
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
