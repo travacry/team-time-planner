ce6b4b2dcafecd3a6847a56fb4a0bb340e1f5c24
André Schreck <andre.schreck@gmail.com>
2018-03-24 12:13:18 +0100

    lunch

---
+++ app/src/app/lunch.ts
@@ -0,0 +1,5 @@
+import { Member } from './member'
+
+export class Lunch {
+    participants: Member[];
+}
--- app/src/app/team.ts
+++ app/src/app/team.ts
@@ -1,6 +1,8 @@
-import { Member } from './member'
+import { Member } from './member';
+import { Lunch } from './lunch';
 
 export class Team {
     name: string;
     members: Member[];
+    lunch: Lunch;
 }
